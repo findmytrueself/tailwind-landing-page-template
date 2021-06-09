0b5e808b0841eed71eb02352533659c9b925cb16
ImHun <load1999@gmail.com>
2021-06-09 15:38:01 +0900

    v1

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import React, { useEffect } from 'react';
-import {
-  Switch,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
 
-import './css/style.scss';
+import "./css/style.scss";
 
-import AOS from 'aos';
-import { focusHandling } from 'cruip-js-toolkit';
+import AOS from "aos";
+import { focusHandling } from "cruip-js-toolkit";
 
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ResetPassword from './pages/ResetPassword';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
     AOS.init({
       once: true,
-      disable: 'phone',
+      disable: "phone",
       duration: 700,
-      easing: 'ease-out-cubic',
+      easing: "ease-out-cubic",
     });
   });
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
-    focusHandling('outline');
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+    focusHandling("outline");
   }, [location.pathname]); // triggered on route change
 
   return (
@@ -41,7 +36,7 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/signin">
+        {/* <Route path="/signin">
           <SignIn />
         </Route>
         <Route path="/signup">
@@ -49,7 +44,7 @@ function App() {
         </Route>
         <Route path="/reset-password">
           <ResetPassword />
-        </Route>
+        </Route> */}
       </Switch>
     </>
   );
